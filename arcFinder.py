4f3ae68e69e99656329a2e552ddab305b57e57ac
Ronald Chiang <rschaing@gmail.com>
2022-03-20 16:29:45 +1100

    Update arcFinder.py

--- arcFinder.py
+++ arcFinder.py
@@ -1,4 +1,4 @@
-from math import atan, sqrt, tan, pi
+from math import atan, sqrt, tan, pi, acos
 
 """ Tests
 (0,0),(0,1),(1,1)
@@ -7,7 +7,9 @@ from math import atan, sqrt, tan, pi
 (0,0),(0,1),(1,2)
 (0,0),(1,1),(2,2)
 (1,0),(0,1),(-1,0)
+(11.05572809000084,24.47213595499958), (20,20), (20,10)
 """
+
 # points are in tuple form (x,y)
 
 def mag(a, b):
@@ -29,39 +31,17 @@ def finder(start, point, end):
     point is the point that is being curved at
     end is the ending point
     """
-    if point[0] == start[0]:
-        m1 = None
-    else:
-        m1 = grad(start, point)
-    
-    if end[0] == point[0]:
-        m2 = None
-    else:
-        m2 = grad(end, point)
-
-    if m1 == m2:
-        angle = 0
-    elif m1 == None:
-        if m2 == 0:
-            angle = pi/2
-        else:
-            angle = pi - atan(abs(1/m2))
-    elif m2 == None:
-        if m1 == 0:
-            angle = pi/2
-        else:
-            angle = pi - atan(abs(1/m1))
-    elif m1*m2 == -1:
-        angle = pi/2
-    else:
-        angle = pi - atan(abs((m1-m2)/(1+m1*m2)))
+    a, b, c = mag(start, point), mag(point, end), mag(start, end)
+    angle = pi - acos((a**2 + b**2 - c**2) / (2 * a * b))
 
     if angle == 0:
         length = mag(start, end)
         curvature = 0
     else:
-        len = mag(start, point)
+        len = a
         radius = len/tan(angle/2)
+        print(angle)
+        print(radius)
         curvature = 1/radius
         length = radius*angle
 
