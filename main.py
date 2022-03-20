92a91a365cd6aabeb95367cbc41fd2d19a83b018
Daniel Yu <yudan2001@gmail.com>
2022-03-20 15:28:48 +1100

    Almost works

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import os
+from lxml import etree
+from roadGenerator import generateRoads
 import json
 
 def getInputCoordinates(filename: str):
     '''
-    Returns a list of tuples (x, y)
+    Returns a list of tuples : [(x, y)]
     '''
     
     with open(filename, "r") as f:
@@ -15,7 +16,34 @@ def getInputCoordinates(filename: str):
         returnList.append(tuple(coord))
         
     return returnList
-    
+
+def createStraightNode(s, x, y, hdg, length):
+    geometry = etree.Element("geometry", s=str(s), x=str(x), y=str(y), hdg=str(hdg), length=str(length))
+    etree.SubElement(geometry, "line")
+    return geometry
+
+def createArcNode(s, x, y, hdg, length, curvature):
+    geometry = etree.Element("geometry", s=str(s), x=str(x), y=str(y), hdg=str(hdg), length=str(length))
+    etree.SubElement(geometry, "arc", curvature=str(curvature))
+    return geometry
+
+def writeToOpenDriveFile(roads, totalLength, openDriveTemplate):
+    with open(openDriveTemplate, "r") as f:
+        content = f.read()
+    root = etree.fromstring(content)
+    tree = etree.ElementTree(root)
+    roadNode = root.xpath("/OpenDRIVE/road")[0]
+    planViewNode = root.xpath("/OpenDRIVE/road/planView")[0]
+    for geometry in roads:
+        if geometry["type"] == "straight":
+            planViewNode.append(createStraightNode(geometry['s'], geometry['x'], geometry['y'], geometry['hdg'], geometry['length']))
+        elif geometry["type"] == "arc":
+            planViewNode.append(createArcNode(geometry['s'], geometry['x'], geometry['y'], geometry['hdg'], geometry['length'], geometry["curvature"]))
+
+    roadNode.set("length", str(totalLength))
+    tree.write("test.xodr", pretty_print=True)
 
 if __name__ == "__main__":
     inputCoordinates = getInputCoordinates("input.json")
+    roads = generateRoads(inputCoordinates)
+    writeToOpenDriveFile(roads[0], roads[1], "openDriveTemplate.xodr")
---
+++ roadGenerator.py
@@ -0,0 +1,78 @@
+from arcStartPt import arcPos, getLen
+from arcFinder import finder
+
+def generateRoads(datapoints):
+    
+    # initialise road points
+    # for straight  {
+    #               "type": "straight", 
+    #               "s": s,
+    #               "x": xStart, 
+    #               "y": yStart, 
+    #               "hdg": heading, 
+    #               "length": length
+    #               }
+    # for arc       {
+    #               "type": "arc",
+    #               "s": s, 
+    #               "x": xStart, 
+    #               "y": yStart, 
+    #               "hdg": heading, 
+    #               "length": length
+    #               "curvature"
+    #               }
+    roads = []
+
+    # get arcs
+    arcPositionsHeading = arcPos(datapoints)
+    arcStarts = [(element[0][0], element[0][1]) for element in arcPositionsHeading] 
+    arcEnds = [(element[1][0], element[1][1]) for element in arcPositionsHeading] 
+    arcHeadings = [element[2] for element in arcPositionsHeading]
+
+    # generate arc length and curvature
+    arcLengthCurvature = list(map(finder, arcStarts, datapoints, arcEnds))
+
+    s = 0
+    lastIndex = len(datapoints) - 1
+    # determine if straight lines are needed and add to road list
+    for i in range(lastIndex):
+        # add arc
+        roads.append(generateArc(s, arcStarts[i][0], arcStarts[i][1], arcHeadings[i], arcLengthCurvature[i][0], arcLengthCurvature[i][1]))
+        s += arcLengthCurvature[i][0]
+        # add straight line if needed
+        if arcEnds[i] != arcStarts[i + 1]:
+            straightLength = getLen(arcEnds[i], arcStarts[i + 1])
+            roads.append(generateStraight(s, arcEnds[i][0], arcEnds[i][1], arcHeadings[i + 1], straightLength))
+            s += straightLength
+    
+    # last arc/straight line to add
+    roads.append(generateArc(s, arcStarts[lastIndex][0], arcStarts[lastIndex][1], arcHeadings[lastIndex], arcLengthCurvature[lastIndex][0], arcLengthCurvature[lastIndex][1]))
+    s += arcLengthCurvature[lastIndex][0]
+    if (arcEnds[lastIndex] != arcStarts[0]):
+        straightLength = getLen(arcEnds[lastIndex], arcStarts[0])
+        roads.append(generateStraight(s, arcEnds[lastIndex][0], arcEnds[lastIndex][1], arcHeadings[0], straightLength))
+        s += straightLength
+
+    return roads, s
+            
+
+def generateStraight(s, x, y, hdg, length):
+    return {
+        "type": "straight",
+        "s": s,
+        "x": x,
+        "y": y,
+        "hdg": hdg,
+        "length": length
+    }
+
+def generateArc(s, x, y, hdg, length, curvature):
+    return {
+        "type": "arc",
+        "s": s,
+        "x": x,
+        "y": y,
+        "hdg": hdg,
+        "length": length,
+        "curvature": curvature
+    }
