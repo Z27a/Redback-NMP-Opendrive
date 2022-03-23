b01d2ef02612cd6c85afe7f825f0711f2d198e41
Ronald Chiang <rschaing@gmail.com>
2022-03-23 12:18:57 +1100

    Update arcFinder.py

--- arcFinder.py
+++ arcFinder.py
@@ -15,9 +15,6 @@ from math import sqrt, tan, pi, acos
 def mag(a, b):
     return sqrt((a[1]-b[1])**2+(a[0]-b[0])**2)
 
-def grad(a, b):
-    return (a[1]-b[1])/(a[0]-b[0])
-
 def orient(a, b, c):
     delta = (b[0]-a[0])*(c[1]-a[1])-(b[1]-a[1])*(c[0]-a[0])
     if delta > 0:
@@ -26,24 +23,14 @@ def orient(a, b, c):
         return False
 
 def finder(start, point, end):
-    """
-    start is the starting point
-    point is the point that is being curved at
-    end is the ending point
-    """
     a, b, c = mag(start, point), mag(point, end), mag(start, end)
     angle = pi - acos((a**2 + b**2 - c**2) / (2 * a * b))
+    curvature = tan(angle/2)/a
 
     if angle == 0:
         length = mag(start, end)
-        curvature = 0
     else:
-        len = a
-        radius = len/tan(angle/2)
-        print(angle)
-        print(radius)
-        curvature = 1/radius
-        length = radius*angle
+        length = a*angle/tan(angle/2)
 
     if orient(start, point, end):
         return (length, curvature)
